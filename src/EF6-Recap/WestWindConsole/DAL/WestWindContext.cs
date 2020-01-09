d755f1382583f4832ebf1a194278df116fe63b0d
Austin Fiddes <austinfiddes@outlook.com>
2020-01-09 16:12:19 -0700

    Adding entities

--- src/EF6-Recap/WestWindConsole/DAL/WestWindContext.cs
+++ src/EF6-Recap/WestWindConsole/DAL/WestWindContext.cs
@@ -10,9 +10,16 @@ namespace WestWindConsole.DAL
 {
     public class WestWindContext : DbContext
     {
+        // By default, Entity Framework (EF) will automatically recreate the database if it doesn't find one where the Connection String is pointing. 
         public WestWindContext() : base("name=WWdb")
         {
             // TODO: Demonstrate null database initializer
+            // We set the database initializer insde the constructor of our DbContext class. 
+            // Setting it to null will prevent Entity Framework from re-creating the database
+            // if it can't find it (which is what we want most of the time).
+            //
+            // An alternative place to "turn off" or disable database initialization is 
+            // in the web.config or app.config file. 
             Database.SetInitializer<WestWindContext>(null);
         }
 
